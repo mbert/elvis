91c821a70204e406dc3c6ba54c7993d05269d569
Martin Dietze <mdietze@gmail.com>
2014-11-28 12:18:23 +0100

    Fixed what I think is a bug: empty statement after `if'.

--- window.c
+++ window.c
@@ -403,7 +403,7 @@ void winresize(win, rows, columns)
 
 #ifdef FEATURE_AUTOCMD
 	/* handle OptChanged event for lines or columns now */
-	if (changed);
+	if (changed)
 		auperform(win, ElvFalse, NULL, AU_OPTCHANGED, changed);
 #endif
 }
