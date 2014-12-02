3a0b9b89676a15c20e6a71c732c09ba36b17e557
Ingo Brückl <ib@wupperonline.de>
2014-12-02 10:37:52 +0100

    Fix and improve 'herefile' script
    Following the << there can be an optional minus sign (not a backslash)
    and whitespace.
    
    Correct \\s.
    
    Trigger OptChanged bufdisplay so that the here-files will be colored
    immediately.

--- data/scripts/herefile.ex
+++ data/scripts/herefile.ex
@@ -3,7 +3,8 @@ color herefile like comment black or white
 aug herefile
 au OptChanged bufdisplay {
   if bufdisplay == "syntax sh" || knownsyntax(filename) == "sh"
-  then try %s#<<\\\?\(\S\+\)#+,/^\\s*\1$/-region herefile \1#x
+  then try %s#<<-\?\s*\(\S\+\)#+,/^\s*\1$/-region herefile \1#x
 }
 au BufDelete all try %unr herefile
 aug END
+let bufdisplay=(bufdisplay)
