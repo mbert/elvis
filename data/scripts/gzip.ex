a9d3a4e7003c4914e1ddb7f75cdb8980ab514426
Ingo Brückl <ib@wupperonline.de>
2014-12-02 10:17:22 +0100

    Fix 'gzip' script
      * :r added .gz to the buffer name (an untitled one became a mere .gz)
      * :e displayed the text in hex mode
      * :e didn't display in syntax (color) mode
      * saving didn't work due to a missing bang
    
    Despite the note in the Announce file, :e worked better in the old
    augz script (the former name of gzip), so the code around rerunning
    "elvis.arf" has been removed again.

--- data/scripts/gzip.ex
+++ data/scripts/gzip.ex
@@ -4,16 +4,15 @@ augroup gzip
   au!
   au BufReadPre,FileReadPre	*.gz set reol=binary
   au BufReadPost		*.gz %!gunzip
-  au FileReadPost		*.gz '[,']!gunzip 2>/dev/null
-  au BufReadPost,FileReadPost	*.gz {
-					set reol=text nomodified
-					file! (basename(filename))
-					source! (elvispath("elvis.arf"))
-					file! (filename).gz
-					set edited
+  au FileReadPost		*.gz '[,']!gunzip
+  au BufReadPost,FileReadPost	*.gz set reol=text nomodified bufdisplay=normal
+  au BufReadPost		*.gz {
+					local s
+					let s = knownsyntax(basename(filename))
+					if s
+					then eval set bufdisplay="syntax (s)"
 				     }
-  au BufReadPost		*.gz eval doau BufReadPost (basename(filename))
-  au BufWritePost,FileWritePost	*.gz eval mv (afile) (basename(afile))
+  au BufWritePost,FileWritePost	*.gz eval !mv (afile) (basename(afile))
   au BufWritePost,FileWritePost	*.gz eval !gzip (basename(afile))
   au FileAppendPre		*.gz eval !gunzip (afile)
   au FileAppendPre		*.gz eval !mv (basename(afile)) (afile)
