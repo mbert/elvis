mbert/elvis

commit a3795a5728a6c2b9a462428385a72c6bf0aacc41

Enhance anchoring by introducing an alternative way to specify the column
Anchoring two comments at the same column, when the comment keywords are
subsets or supersets of each other, will fail, because the shorter comment
keyword will always win. This is because the keywords are searched in
ascending order.

  Example:

  comment -
  comment --
  anchor 1 - --
  font a -
  font b --

  All lines starting with -- will be in font a, too!

To solve this problem, there is an alternative way to specify the column now.
A column number prefixed by a ~ character will be preferred to the same
column number without prefix.

  Example:

  comment -
  comment --
  anchor 1 -
  anchor ~1 --
  font a -
  font b --

  All lines starting with -- will be in font b now!

As a drawback, the maximum column number that can be used diminishes from
4094 (0xFFE) to 2046 (0x7FE).

dmsyntax.c
@@ -93,8 +93,9 @@ typedef struct
 #define wordbeforeregsub(w)	(wordflags(w) & USEREGSUB)
 #define wordoperator(w)		(wordflags(w) & OPERATOR)
 
-/* Most anchor values are in the range 1 - 0xffe, but these are special */
+/* Most anchor values are in the range 1 - 0x7fe, but these are special */
 #define ANCHOR_NONE	0
+#define ANCHOR_ALTERNATIVE	0x800
 #define ANCHOR_FRONT	0xfff
 
 #if USE_PROTOTYPES
@@ -136,6 +137,7 @@ static spell_t *scankeyword(refp, colplusone, indent)
 	ELVBOOL	indent;	/* preceded only by indentation whitespace? */
 {
 	spell_t *node;
+	spell_t *nnode = NULL;	/* node for normal colplusone anchor */
 	int	anchor;
 
 	/* look it up, being careful about case sensitivity */
@@ -168,7 +170,13 @@ static spell_t *scankeyword(refp, colplusone, indent)
 
 				  default:
 					/* must be in a specific column */
-					if (anchor != colplusone)
+					if (anchor == colplusone)
+					{
+						nnode = node;	/* keep node for normal anchor in mind... */
+						goto Continue;	/* ...but continue searching for... */
+					}
+					/* ...an alternative one which is preferred */
+					else if (anchor != (colplusone | ANCHOR_ALTERNATIVE))
 					{
 						goto Continue;
 					}
@@ -186,6 +194,7 @@ Continue:
 		else
 			node = spellletter(node, **refp);
 	}
+	if (!SPELL_IS_GOOD(node)) node = nnode;	/* no node for alternative anchor, try normal one */
 	if (!SPELL_IS_GOOD(node))
 		return NULL;
 
@@ -580,6 +589,8 @@ static DMINFO *init(win)
 				{
 					if (values[1][0] == '^')
 						j = ANCHOR_FRONT;
+					else if (values[1][0] == '~')
+						j = atoi(tochar8(values[1]) + 1) | ANCHOR_ALTERNATIVE;
 					else
 						j = atoi(tochar8(values[1]));
 					if (j > 0 && j <= ANCHOR_FRONT)

doc/elvisdm.html
@@ -233,6 +233,11 @@ can only appear at the front of a line
 (after optional whitespace but no printed characters).
 The remainder of the "anchor" line is the list
 of keywords which are only special when they occur in that column.
+<p>If the column number is prefixed by ~ then the keyword gets preference to
+another keyword defined with the same column number. This is useful for
+<a href="#comment">comment</a> keywords which are subsets or supersets of
+each other, because the shorter comment keyword will always win else.
+With ~ the longer one will be considered first.
 
 <dt><a name="prefix">prefix</a>
 <dd>This defines keywords which should be recognized even if they are

commit 980729b7248bd88f1fefbd118c8bdee88f77aa2c

Fix segmentation fault with column based anchoring
Commit a3795a5728a6c2b9 introduced an alternative way to specify the
column for an anchor.

While searching for a node for an alternative anchor the reference
pointer to the end of the scanned text will move further, and if there
is no such node the reference pointer belonging to the node for the
normal anchor will be lost.

Save the reference pointer belonging to the node for the normal anchor
and restore it when returning this very node.

This closes issue #16 reported by fbrau on GitHub.

dmsyntax.c
@@ -138,6 +138,7 @@ static spell_t *scankeyword(refp, colplusone, indent)
 {
 	spell_t *node;
 	spell_t *nnode = NULL;	/* node for normal colplusone anchor */
+	CHAR    *rrefp;		/* reference for normal colplusone anchor */
 	int	anchor;
 
 	/* look it up, being careful about case sensitivity */
@@ -173,6 +174,7 @@ static spell_t *scankeyword(refp, colplusone, indent)
 					if (anchor == colplusone)
 					{
 						nnode = node;	/* keep node for normal anchor in mind... */
+						rrefp = *refp;
 						goto Continue;	/* ...but continue searching for... */
 					}
 					/* ...an alternative one which is preferred */
@@ -194,7 +196,11 @@ Continue:
 		else
 			node = spellletter(node, **refp);
 	}
-	if (!SPELL_IS_GOOD(node)) node = nnode;	/* no node for alternative anchor, try normal one */
+	if (!SPELL_IS_GOOD(node) && nnode)	/* no node for alternative anchor, try normal one */
+	{
+		node = nnode;
+		*refp = rrefp;
+	}
 	if (!SPELL_IS_GOOD(node))
 		return NULL;
 