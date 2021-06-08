9402231e8811cf3ec32398620d83551977d06eee
Malcolm <malxau@users.noreply.github.com>
2021-06-07 22:31:21 -0700

    Remove makefiles which seem superseded

--- makwin32.bat
+++ makwin32.bat
@@ -13,11 +13,8 @@ if "%INCLUDE%"=="" call vcvars32
 
 echo Configuring...
 if not exist make.bat copy makwin32.bat make.bat
-if not exist ctags.mak copy oswin32\ctags.mak
 if not exist elvis.mak copy oswin32\elvis.mak
 if not exist elvisutl.mak copy oswin32\elvisutl.mak
-if not exist fmt.mak copy oswin32\fmt.mak
-if not exist ref.mak copy oswin32\ref.mak
 if not exist winelvis.mak copy oswin32\winelvis.mak
 if not exist wintags.mak copy oswin32\wintags.mak
 if not exist config.h copy oswin32\osconfig.h config.h
